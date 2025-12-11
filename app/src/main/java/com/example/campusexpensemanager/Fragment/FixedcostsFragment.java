46b76ad2949723889d4a1737000514695f44931b
Canh26092005 <vucanh2609@gmail.com>
2025-12-11 17:55:07 +0700

    layouthome

--- app/src/main/java/com/example/campusexpensemanager/Fragment/FixedcostsFragment.java
+++ app/src/main/java/com/example/campusexpensemanager/Fragment/FixedcostsFragment.java
@@ -7,11 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.example.campusexpensemanager.R;
 
-public class BudgetFragment extends Fragment {
+public class FixedcostsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_budget, container, false);
+        return inflater.inflate(R.layout.fragment_fixedcosts, container, false);
     }
 }
